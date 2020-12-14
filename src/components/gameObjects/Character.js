4ba811f9d744804f07ec85cca204edf580e46c73
Zef <me@zef.sh>
2020-12-14 21:08:48 +0700

    Add basic character animation and rotation

--- src/components/gameObjects/Character.js
+++ src/components/gameObjects/Character.js
@@ -5,69 +5,120 @@ import Globals from '../../Globals';
 class Character extends Component {
     constructor(props) {
         super(props);
-        this.object = undefined;
-        this.meshes = undefined;
+        this.mesh = undefined;
+        this.characterModel = undefined;
+        this.characterModelMeshes = undefined;
         
+        // Object position offset
         this.xOffset = 0.25;
         this.yOffset = -0.5;
         this.zOffset = 0;
 
-        this.animationSpeed = 0.5;  // Speed in seconds
+        // Animations
+        this.animationSpeed = 0.5;
+        this.turnLeftAnimation = undefined;
 
+        // Put a global reference to this object
         Globals.character = this;
     }
     
     componentDidMount = () => {
         this.createMesh();
-        // this.initializeAnimations();
     }
-
+    
     componentWillUnmount = () => {
-        this.meshes.forEach(mesh => {
-            mesh.dispose();
-        });
+        Globals.character = undefined;
+        this.mesh.dispose();
     }
-
+    
     createMesh = () => {
         // Read values from component props. Load default value if not defined.
         const posX = this.props.posX ? this.props.posX : 0;
         const posY = this.props.posY ? this.props.posY : 0;
         const posZ = this.props.posZ ? this.props.posZ : 0;
+        
+        // Create parent mesh
+        this.mesh = BABYLON.MeshBuilder.CreateBox(
+            "character",
+            { height: 1, width: 1, depth: 1 },
+            Globals.scene
+        );
+
+        // Create parent material for transparency
+        const characterParentMaterial = new BABYLON.Material("characterParentMaterial");
+        characterParentMaterial.alpha = 0;
+            
+        // Modify parent object properties
+        this.mesh.position = new BABYLON.Vector3(posX, posY, posZ);
+        this.mesh.material = characterParentMaterial;
+        
 
         // Load mesh
-        this.object = BABYLON.SceneLoader.ImportMesh(
+        this.characterModel = BABYLON.SceneLoader.ImportMesh(
             "",
             "/assets/obj/",
-            "character.glb",
+            "character_running.glb",
             Globals.scene,
-            (meshes) => {
+            (meshes) => {               
                 // Expose the meshes to the outside world
-                this.meshes = meshes;
-
+                this.characterModelMeshes = meshes;
+                
                 // Modify mesh properties
                 meshes.forEach(mesh => {
-                    console.log(this.yOffset)
+                    mesh.scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
                     mesh.position = new BABYLON.Vector3(
                         posX + this.xOffset,
                         posY + this.yOffset,
                         posZ + this.zOffset
                     );
-                    mesh.scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+                    mesh.setParent(this.mesh);
                 });
             }
         );
     }
 
     moveCharacter = () => {
-        this.meshes.forEach(mesh => {
-            mesh.position.x += 1;
+        this.mesh.position.x += 1;
+        // let runningAnimation = Globals.scene.getAnimationGroupByName("Running");
+        // runningAnimation.stop();
+    }
+
+    turnLeft = () => {
+        console.log(this.mesh);
+
+        const turnLeftAnimation = new BABYLON.Animation(
+            "characterTurnleft",
+            "rotation.y",
+            Globals.framerate,
+            BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+            BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+        );
+
+        const turnLeftKeyframes = [];
+        turnLeftKeyframes.push({
+            frame: 0,
+            value: this.mesh.rotation.y
         });
+        turnLeftKeyframes.push({
+            frame: this.animationSpeed * Globals.framerate,
+            value: this.mesh.rotation.y + Math.PI/2
+        });
+
+        turnLeftAnimation.setKeys(turnLeftKeyframes);
+        
+        // Pop the last animation before adding another one
+        if (this.mesh.animations.length > 0)
+            this.mesh.animations.pop();
+        this.mesh.animations.push(turnLeftAnimation);
+
+        Globals.scene.beginAnimation(this.mesh, 0, this.animationSpeed * Globals.framerate, false);
     }
     
     render() {
         return (
             <div>
                 <button onClick={this.moveCharacter}>PRESS ME DADDY</button>
+                <button onClick={this.turnLeft}>Turn Left</button>
             </div>
         )
     }

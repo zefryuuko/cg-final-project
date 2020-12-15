d428fd55526f0d59581890e64d6ddeaf17138f13
Zef <me@zef.sh>
2020-12-15 19:52:36 +0700

    Create code engine basic interface

--- src/components/codeEngine/CodeEngine.js
+++ src/components/codeEngine/CodeEngine.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react'
 
 class CodeEngine extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        // const { allowedFunctions, level } = props;
+        this.state = {
+            functions: [],
+            isRunning: false
+        }
+
+        // Block types enum
+        this.CODE_BLOCK_TYPE = Object.freeze({
+            WALK: 0,
+            TURN: 1,
+            IF: 2,
+            LOOP: 3
+        });
+    }
+
+    componentDidMount = () => {
+
+    }
+
+    startSimulation = () => {
+
+    }
+
+    addBlock = (blockType) => {
+
+    }
+
+    render = () => {
         return (
             <div className="codeEngine">
-                placeholder
+                <div className="block function">
+                    function
+                </div>
+                <div className="block if">
+                    if
+                </div>
+                <div className="block loop">
+                    loop
+                </div>
             </div>
-        )
+        );
     }
 }
 
-export default CodeEngine
+export default CodeEngine;

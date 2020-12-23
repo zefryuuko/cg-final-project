9c9fbfc63175baf0c4c48cb4ed253c9fef00f3cc
Zef <me@zef.sh>
2020-12-23 22:34:25 +0700

    Shut up eslint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'babylonjs-loaders';
 import BabylonCanvas from './BabylonCanvas';
 import AddBlockModal from './components/uiElements/AddBlockModal';
